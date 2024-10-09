69471b41eacdebc2b91b839c09ad49ef8b325b12
Agarvin <agarvin2002@gmail.com>
2024-10-09 10:13:08 +0530

    changed email to username in common  and fixed CROS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,19 @@
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { Hono } from 'hono';
-import { sign } from 'hono/jwt'
-import { userRouter } from './routes/user';
-import { blogRouter } from './routes/blog';
+import { Hono } from "hono";
+import { userRouter } from "./routes/user";
+import { blogRouter } from "./routes/blog";
+import { cors } from "hono/cors";
 
 // Create the main Hono app
 const app = new Hono<{
-	Bindings: {
-		DATABASE_URL: string,
-		JWT_SECRET: string,
-	}
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
 }>();
 
-app.route('/api/v1/user', userRouter);
-app.route('/api/v1/blog', blogRouter);
+app.use("/*", cors());
 
-export default app
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
-
-
+export default app;
